55a140f89e25c6fafb39cfef96668e9a7310c591
jonesna213 <navyjones9@gmail.com>
2023-12-18 17:21:28 -0600

    Add project files.

---
+++ SimpleFitness.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleFitness.UI.Startup))]
+namespace SimpleFitness.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
