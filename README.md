cd9b5639504a6105cfcc538ea9828bf0562dd2c5
jonesna213 <navyjones9@gmail.com>
2023-12-18 18:43:03 -0600

    Did some initial setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SimpleFitness
+
+## Goal
+The goal of this project is to create an application where you can input your goals, height, age, weight, etc and be given a
+workout plan and meal plan which will be created by OpenAI and saved to your account. (can be recreated as well with more criteria) 
+In addition to this you will be able to log your Macros by simply clicking 
+the corresponding button for what macro you want to add.
+
+## Technologies
+
+I am doing this project in C#/.NET in order to get better at the technology.
---
+++ SimpleFitness.UI/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleFitness.UI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SimpleFitness.UI.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SimpleFitness.UI.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
